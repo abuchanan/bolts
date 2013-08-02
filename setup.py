c4c115a62449c57388b5f2683b06d19aa9113c5c
Alex Buchanan <buchanae@gmail.com>
2013-08-02 10:06:26 -0700

    make bolts a package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name='bolts',
+    description="Nuts and bolts. Small recipes I've found occasionally useful.",
+    long_description=open('README.md').read(),
+    version='1.0.0',
+    author='Alex Buchanan',
+    author_email='buchanae@gmail.com',
+    license='Apache',
+    py_modules=['bolts'],
+)
